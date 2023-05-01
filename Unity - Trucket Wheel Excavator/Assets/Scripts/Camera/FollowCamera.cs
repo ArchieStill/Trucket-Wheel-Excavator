497011781a127cc93c5c6132a663e533a1e4bd5b
ArchieStill <archie2.still@live.uwe.ac.uk>
2023-05-01 19:07:46 +0100

    worksheet 3 camera done

--- Unity - Trucket Wheel Excavator/Assets/Scripts/Camera/FollowCamera.cs
+++ Unity - Trucket Wheel Excavator/Assets/Scripts/Camera/FollowCamera.cs
@@ -6,6 +6,9 @@ public class FollowCamera : MonoBehaviour
 {
     public GameObject target;
     Vector3 offset;
+    private float mouseX;
+    private float mouseY;
+    private float mouseZ;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,32 @@ public class FollowCamera : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
         transform.position = target.transform.position + (rotation * offset);
         transform.LookAt(target.transform);
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        mouseZ = Input.GetAxis("Mouse ScrollWheel");
+        float angleBetween = Vector3.Angle(Vector3.up, transform.forward);
+        Debug.Log("offset angle: " + angleBetween);
+
+        if (Input.GetMouseButton (1))
+        {
+            offset = Quaternion.Euler(0, mouseX, 0) * offset;
+        }
+        if (((angleBetween > 90) && (mouseY < 0)) || ((angleBetween < 140) && (mouseY > 0)))
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 LocalRight = target.transform.worldToLocalMatrix.MultiplyPoint(transform.right);
+                offset = Quaternion.AngleAxis(mouseY, LocalRight) * offset;
+            }
+        }
+        if (mouseZ < 0)
+        {
+            offset = Vector3.Scale(offset, new Vector3(1.05f, 1.05f, 1.05f));
+        }
+        if (mouseZ > 0)
+        {
+            offset = Vector3.Scale(offset, new Vector3(0.95f, 0.95f, 0.95f));
+        }
     }
 }
